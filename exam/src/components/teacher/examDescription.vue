fc66cd60527bf1b9894693e58aec6014a9503e51
Lanyy11790 <2932008657@qq.com>
2026-04-25 14:46:09 +0800

    “第一次提交”

---
+++ exam/src/components/teacher/examDescription.vue
@@ -0,0 +1,29 @@
+<!-- 考试管理功能介绍 -->
+<template>
+  <section class="description">
+    <p class="title">考试管理功能介绍</p>
+    <p class="content">考试管理功能是实现考试全流程数字化、智能化的核心模块，适配学校、企业、培训机构等多场景，涵盖题库、组卷、考试、监考、阅卷、成绩分析全环节，高效解决传统考试组织繁琐、效率低下等问题。
+核心功能包括：题库管理支持多题型录入、多维分类及批量导入导出，方便试题维护与检索；组卷可灵活选择固定、随机等模式，搭配试卷参数设置，兼顾公平与便捷。考试发布可精准划定对象、安排时间，多端适配满足不同考试场景；AI监考结合身份核验、环境监控等防作弊手段，保障考试公正。
+阅卷环节实现客观题自动判分、主观题在线评阅，成绩自动合成；多维度数据分析与报表导出，助力精准评估效果。整体功能精简人工操作，提升考试组织效率，兼顾公平性与灵活性，为教学改进、人才选拔提供数据支撑。
+    </p>
+  </section>
+</template>
+
+<style lang="less" scoped>
+.description {
+  margin-left: 40px;
+  .title {
+    font-size: 22px;
+    font-weight: 400;
+    color: rgb(31, 47, 61);
+  }
+  .content {
+    width: 600px;
+    background-color: rgb(236, 248, 255);
+    padding: 16px 32px;
+    border-radius: 4px;
+    border-left: 5px solid rgb(80, 191, 255);
+    margin: 20px 0px;
+  }
+}
+</style>
